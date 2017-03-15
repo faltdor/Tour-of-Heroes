14b7451d902b90e906f3553cee9903ecb204218a
faltdor <faltdor@gmail.com>
2017-03-15 01:04:02 -0500

    refactored HeroService to load heroes from a , API.,extended HeroService to support post, put and delete methods.updated our components to allow adding, editing and deleting of heroes.configured an in-memory web API.

--- src/app/hero.service.js
+++ src/app/hero.service.js
@@ -9,11 +9,27 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var http_1 = require('@angular/http');
+require('rxjs/add/operator/map');
+require('rxjs/add/operator/toPromise');
 var mock_heroes_1 = require('./mock-heroes');
 var HeroService = (function () {
-    function HeroService() {
+    function HeroService(http) {
+        this.http = http;
+        this.heroesUrl = 'api/heroes'; // URL to web api
+        this.headers = new http_1.Headers({ 'Content-Type': 'application/json' });
     }
     HeroService.prototype.getHeroes = function () {
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(function (response) { return response.json().data; })
+            .catch(this.handleError);
+    };
+    HeroService.prototype.handleError = function (error) {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
+    };
+    HeroService.prototype.getHeroes2 = function () {
         return Promise.resolve(mock_heroes_1.HEROES);
     }; // stub
     HeroService.prototype.getHeroesSlowly = function () {
@@ -23,13 +39,47 @@ var HeroService = (function () {
             setTimeout(function () { return resolve(_this.getHeroes()); }, 2000);
         });
     };
-    HeroService.prototype.getHero = function (id) {
+    HeroService.prototype.getHero2 = function (id) {
         return this.getHeroes()
             .then(function (heroes) { return heroes.find(function (hero) { return hero.id === id; }); });
     };
+    HeroService.prototype.getHero = function (id) {
+        var url = this.heroesUrl + "/" + id;
+        return this.http.get(url)
+            .toPromise()
+            .then(function (response) { return response.json().data; })
+            .catch(this.handleError);
+    };
+    HeroService.prototype.update = function (hero) {
+        var url = this.heroesUrl + "/" + hero.id;
+        return this.http
+            .put(url, JSON.stringify(hero), { headers: this.headers })
+            .toPromise()
+            .then(function () { return hero; })
+            .catch(this.handleError);
+    };
+    HeroService.prototype.create = function (name) {
+        return this.http
+            .post(this.heroesUrl, JSON.stringify({ name: name }), { headers: this.headers })
+            .toPromise()
+            .then(function (res) { return res.json().data; })
+            .catch(this.handleError);
+    };
+    HeroService.prototype.delete = function (id) {
+        var url = this.heroesUrl + "/" + id;
+        return this.http.delete(url, { headers: this.headers })
+            .toPromise()
+            .then(function () { return null; })
+            .catch(this.handleError);
+    };
+    HeroService.prototype.search = function (term) {
+        return this.http
+            .get("app/heroes/?name=" + term)
+            .map(function (response) { return response.json().data; });
+    };
     HeroService = __decorate([
         core_1.Injectable(), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [http_1.Http])
     ], HeroService);
     return HeroService;
 }());
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import { Observable }     from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -6,8 +11,23 @@ import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+	private heroesUrl = 'api/heroes';  // URL to web api
 
-	getHeroes(): Promise<Hero[]> {
+	constructor(private http: Http) { }
+
+	 getHeroes(): Promise<Hero[]> {
+	    return this.http.get(this.heroesUrl)
+	               .toPromise()
+	               .then(response => response.json().data as Hero[])
+	               .catch(this.handleError);
+	  }
+
+	  private handleError(error: any): Promise<any> {
+	    console.error('An error occurred', error); // for demo purposes only
+	    return Promise.reject(error.message || error);
+	  }
+
+	getHeroes2(): Promise<Hero[]> {
 		return Promise.resolve(HEROES);
 	} // stub
 
@@ -18,8 +38,50 @@ export class HeroService {
 	  });
 	}
 
-	getHero(id: number): Promise<Hero> {
+	getHero2(id: number): Promise<Hero> {
 	  return this.getHeroes()
 	             .then(heroes => heroes.find(hero => hero.id === id));
 	}
+
+	getHero(id: number): Promise<Hero> {
+	  const url = `${this.heroesUrl}/${id}`;
+	  return this.http.get(url)
+	    .toPromise()
+	    .then(response => response.json().data as Hero)
+	    .catch(this.handleError);
+	}
+
+	private headers = new Headers({'Content-Type': 'application/json'});
+
+	update(hero: Hero): Promise<Hero> {
+	  const url = `${this.heroesUrl}/${hero.id}`;
+	  return this.http
+	    .put(url, JSON.stringify(hero), {headers: this.headers})
+	    .toPromise()
+	    .then(() => hero)
+	    .catch(this.handleError);
+	}
+
+	create(name: string): Promise<Hero> {
+	  return this.http
+	    .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+	    .toPromise()
+	    .then(res => res.json().data)
+	    .catch(this.handleError);
+	}
+
+	delete(id: number): Promise<void> {
+	  const url = `${this.heroesUrl}/${id}`;
+	  return this.http.delete(url, {headers: this.headers})
+	    .toPromise()
+	    .then(() => null)
+	    .catch(this.handleError);
+	}
+
+	  search(term: string): Observable<Hero[]> {
+	    return this.http
+	               .get(`app/heroes/?name=${term}`)
+	               .map(response => response.json().data as Hero[]);
+ 	 }
+
 }
